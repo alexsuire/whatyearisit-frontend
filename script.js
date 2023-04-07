ceac837c5a0be72725085d56c8f0633b62084964
alexandresuire <alexandresuire75002@gmail.com>
2023-04-07 11:10:41 +0200

    yo

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ fetch('https://whatyearisit-backend-iota-six.vercel.app/year'
 	.then(response => response.json())
 	.then(data => {
 		document.querySelector("#year").textContent = ` 
-        ${data.year}
+        "${data.year}"
         `
 	});
